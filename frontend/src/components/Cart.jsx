a7cce9dcd7786ae022a5ab5afee0f7c94f9980f9
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-22 11:37:00 +0530

    show total quantity in badge with animation

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -3,11 +3,13 @@ import ItemCartCard from "./ItemCartCard";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { MdCurrencyRupee } from "react-icons/md";
 
 const Cart = () => {
-  const [activeCart, setActiveCart] = useState(true);
-
+  const [activeCart, setActiveCart] = useState(false);
   const cartItems = useSelector((state) => state.cart.cart);
+
+  const totalQty = cartItems.reduce((totalQty, item) => totalQty + item.qty, 0);
   return (
     <>
       <aside
@@ -32,19 +34,32 @@ const Cart = () => {
         )}
 
         <div className="absolute bottom-0">
-          <h3 className="font-semibold text-gray-800">Item:</h3>
-          <h3 className="font-semibold text-gray-800">Total Amount:</h3>
+          <h3 className="font-semibold text-gray-800">Item: {totalQty}</h3>
+          <h3 className="font-semibold text-gray-800 flex items-center">
+            Total Amount: <MdCurrencyRupee />
+            {0}
+          </h3>
           <hr className="w-[88vw] lg:w-[22.3vw] my-2" />
           <button className="font-bold bg-green-500 text-white px-3 py-2 rounded-md w-[88vw] lg:w-[22.3vw] mb-5">
             Check Out
           </button>
         </div>
       </aside>
-      <div onClick={() => setActiveCart(!activeCart)}>
-        <FaShoppingCart className="text-5xl bg-white rounded-full shadow-md p-3 fixed bottom-2 right-2" />
-        <span className="flex justify-center items-center text-xs font-bold text-white bg-red-600 w-4 h-4 rounded-full p-1 fixed bottom-11 right-3 z-20">
-          {cartItems.length}
-        </span>
+
+      <div
+        onClick={() => setActiveCart(!activeCart)}
+        className={`${totalQty > 0 ? "animate-bounce" : ""} fixed bottom-2 right-2 cursor-pointer`}
+      >
+        <FaShoppingCart
+          fill="white"
+          className="text-5xl bg-green-500 rounded-full shadow-md p-3"
+        />
+
+        {totalQty > 0 && (
+          <span className="absolute -top-1 -right-1 flex justify-center items-center text-xs font-bold text-white bg-red-600 w-5 h-5 rounded-full">
+            {totalQty}
+          </span>
+        )}
       </div>
     </>
   );
