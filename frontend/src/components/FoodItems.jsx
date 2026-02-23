3dce2c771253280df5de9f84e2ab7496da69d193
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-23 11:43:03 +0530

    Implemented dynamic food filtering using Redux selected category and conditional rendering of FoodCard components.

--- frontend/src/components/FoodItems.jsx
+++ frontend/src/components/FoodItems.jsx
@@ -1,10 +1,21 @@
 import FoodCard from "./FoodCard";
 import FoodData from "../data/foodData";
+import { useSelector } from "react-redux";
 
 const FoodItems = () => {
+  const selectedCategory = useSelector((state) => state.category.category);
+  const filterFoodByCategories = FoodData.filter((food) => {
+    if (selectedCategory === "All") {
+      return food;
+    } else {
+      return selectedCategory === food.category;
+    }
+  });
+
+  console.log(filterFoodByCategories);
   return (
     <section className="flex  flex-wrap gap-6 justify-center mx-6 lg:justify-start mt-10 mb-20">
-      {FoodData.map((foodItem) => {
+      {filterFoodByCategories.map((foodItem) => {
         return <FoodCard key={foodItem.id} foodData={foodItem} />;
       })}
     </section>
