62424db85e25fdb7cb1a88946709ed0e2932695f
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-24 11:29:36 +0530

    configure CORS middleware for frontend integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 import express from "express";
 import paymentRouter from "./routes/payment.routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1", paymentRouter);
