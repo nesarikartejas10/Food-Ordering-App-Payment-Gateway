cc15929e1087475c18ca19d078a593cfddcea701
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-24 08:52:53 +0530

    create account on razorpay and generate api key and api secret

--- backend/src/config/envConfig.js
+++ backend/src/config/envConfig.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 const _config = {
   port: process.env.PORT,
+  razorpayApiKey: process.env.RAZORPAY_API_KEY,
+  razorpayApiSecret: process.env.RAZORPAY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
