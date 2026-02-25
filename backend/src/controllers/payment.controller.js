66eaf4265fa7c6db16e136d27ca025ebdb4b6584
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-25 07:03:30 +0530

    implement HMAC-based Razorpay payment verification

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,4 +1,6 @@
+import { config } from "../config/envConfig.js";
 import { razorpayInstance } from "../config/razorpay.js";
+import crypto from "node:crypto";
 
 export const createOrder = (req, res) => {
   const { amount } = req.body;
@@ -21,4 +23,23 @@ export const createOrder = (req, res) => {
   } catch (error) {
     return res.status(500).json({ success: false });
   }
+};
+
+export const verifyPayment = async (req, res) => {
+  const { order_id, payment_id, signature } = req.body;
+
+  const secret = config.razorpayApiSecret;
+
+  const hmac = crypto.createHmac("sha256", secret);
+  hmac.update(order_id + "|" + payment_id);
+
+  const generatedSignature = hmac.digest("hex");
+
+  if (generatedSignature === signature) {
+    return res.status(200).json({ success: true, message: "Payment verified" });
+  }
+  return res.status(400).json({
+    success: false,
+    message: "Invalid signature! Payment not verified",
+  });
 };
